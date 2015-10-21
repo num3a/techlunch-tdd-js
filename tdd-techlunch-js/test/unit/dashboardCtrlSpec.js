cc4bd28dbd4bc4494b6e44aa89ea187ba0e7dddd
Emmanuel Ernest <ernest.emmanuel@hotmail.fr>
2015-10-21 22:44:03 +0200

    Initialize dashboard controller

---
+++ tdd-techlunch-js/test/unit/dashboardCtrlSpec.js
@@ -0,0 +1,31 @@
+﻿/*global describe, it, beforeEach, inject, expect*/
+(function () {
+    'use strict';
+
+    describe('Dashboard Controller', function () {
+        var ctrl, scope;
+
+        // Load the module containing the app, only 'ng' is loaded by default.
+        beforeEach(module('todomvc'));
+
+
+        beforeEach(inject(function ($controller, $rootScope, localStorage) {
+            scope = $rootScope.$new();
+
+            ctrl = $controller('DashboardCtrl', {
+                $scope: scope
+            });
+        }));
+
+    
+        describe('the filter', function () {
+            it('should default to ""', function () {
+
+
+            });
+
+         
+        });
+
+    });
+}());
